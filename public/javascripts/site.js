b6284a30050db430a876c529309b074ef34ba535
hyang77 <hyang77@hawk.iit.edu>
2020-09-24 15:12:25 -0500

    fix socket connection

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -2,4 +2,6 @@ var socket = io.connect("/");
 
 socket.on("message", function (data) {
   console.log("Message received: " + data);
+  //test if client received message
+  socket.emit('message received', 'Yea, I got your message');
 });
