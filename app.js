d8bb3884c31e97c0e9ecd1bca2822366d3469127
Unai Rada <unairadad@gmail.com>
2020-09-25 20:11:15 -0500

    Log added or removed lines in file.txt whenever it changes

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ const logger = require("morgan");
 const io = require("socket.io")();
 
 //Add fs module
-const fs = require("fs")
+const fs = require("fs");
 var old_file = fs.readFileSync('./var/file.txt', {encoding:"utf8"});
+const diff = require("diff");
 
 const indexRouter = require("./routes/index");
 
@@ -24,6 +25,17 @@ fs.watch('./var/file.txt', function(eventType, filename){
     var new_file = data;
     if (new_file !== old_file) {
       console.log(`The content of ${filename} has changed. It was a ${eventType} event.`);
+      var file_changes = diff.diffLines(old_file, new_file);
+      console.log(`Here are the changes:`);
+      file_changes.forEach((change, i) => {
+        if (change.added){
+          console.log(`Added: ${change.value}`);
+        }
+        if (change.removed){
+          console.log(`Removed: ${change.value}`);
+        }
+      });
+
     }
     old_file = new_file
   });
