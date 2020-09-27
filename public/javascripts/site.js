b8072716c42d5d33c1824bb1c9304e1e3e465c74
Unai Rada <unairadad@gmail.com>
2020-09-26 21:53:38 -0500

    add condition to only emit 'Yeah I got your message' on the successful socket connection

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -3,5 +3,8 @@ var socket = io.connect("/");
 socket.on("message", function (data) {
   console.log("Message received: " + data);
   //test if client received message
-  socket.emit('message received', 'Yea, I got your message');
+  if(data == 'Successfully connected.'){
+    socket.emit('message received', 'Yeah, I got your message');
+  }
+
 });
