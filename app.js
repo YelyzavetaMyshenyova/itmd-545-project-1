576f627d08f288325f889ddc91ec121469d3bb06
YelyzavetaMyshenyova <46732280+YelyzavetaMyshenyova@users.noreply.github.com>
2020-10-07 21:13:43 -0500

    adding changes after everybody elses changes (#3)

--- app.js
+++ app.js
@@ -6,15 +6,12 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const io = require("socket.io")();
-//Add fs module
 const fs = require("fs");
-//Add diff module
 const diff = require("diff");
-//Add EventEmitter class
 const {EventEmitter} = require('events');
-
 const axios = require('axios');
 const cheerio = require('cheerio');
+const schedule = require('node-schedule');
 
 const indexRouter = require("./routes/index");
 const subscriptionRouter = require('./routes/subscription');
@@ -23,100 +20,115 @@ const webPush = require('web-push')
 const app = express();
 
 
-var old_file = fs.readFileSync('./var/file.txt', {encoding:"utf8"});
+var old_file = fs.readFileSync('./var/weatherData.json', {encoding:"utf8"});
 var fileEvent = new EventEmitter();
 
-
-//requesting html from the webpage
-axios.get('https://www.weather.gov/ilx/maxtemp')
-  .then((res) => {
-    if (res.status === 200){
-      const html = res.data;
-      //load html into cheerio
-      const $ = cheerio.load(html);
-      //loading weather infomation
-      const weatherInfo = $('pre');
-      const output = weatherInfo.html();
-      //console.log(output);
-      fs.writeFile('./var/file.txt', output, error => {
-        //In case of error throw err exception
-        if (error) throw err;
-      });
-    }
-  })
-  .catch((error) => {
-    console.log('Fail to fetch', error);
-  })
-
 //Diff testing locally
-fs.watch('./var/file.txt', function(eventType, filename){
+fs.watch('./var/weatherData.json', function(eventType, filename){
   fs.promises.readFile(`./var/${filename}`,{encoding:"utf8"})
   .then(function(data) {
-    //console.log(`The file has this content:\n\n${data}`);
-    //console.log(data); This logs the data without string format.
-    //To be able to see it as a string representation, add "{encoding: "utf8"}" object before the callback function
+
+
+        //notification
+        const vapid_keys = {
+            public: 'BMJ9IpyHYOZIPP4fkxbmu_rd3CD95Bw_ehAJc8KSyvR04QWU78xHOw9A0e07OYwPA4bO2SjF_BT0Z1xYViLSZbI',
+            private: 'uhONt3RMY8ooDWp1vZ15_aWojSCcbumeJ27FaTx5tlM',
+        };
+        webPush.setVapidDetails(
+            'mailto:lizamyshenyova@gmail.com',
+            vapid_keys.public,
+            vapid_keys.private
+        );
+
+        fs.promises.readFile(`var/subscription.json`, {encoding:"utf8"})
+            .then(function(subs) {
+              let subscription = subs.split('\n');
+              subscription.map(function(subscription) {
+                if (subscription.length > 5) {
+                  subscription = JSON.parse(subscription);
+                  console.log('Subscription to send to:', subscription);
+                  console.log('Message to send:', new_file);
+
+                    webPush.sendNotification(subscription, 'The weather report has changed')
+               .catch(function(error) {
+                    console.error('sendNotification error: ', error, subscription, new_file);
+                });
+
+
+            }
+          });
+        })
+        .catch(function(error) {
+             console.error('Error: ', error);
+        });
+    //parse json string and get temperature differences bewteen old data and new data
     var new_file = data;
-    const vapid_keys = {
-        public: 'BMJ9IpyHYOZIPP4fkxbmu_rd3CD95Bw_ehAJc8KSyvR04QWU78xHOw9A0e07OYwPA4bO2SjF_BT0Z1xYViLSZbI',
-        private: 'uhONt3RMY8ooDWp1vZ15_aWojSCcbumeJ27FaTx5tlM',
-    };
-    webPush.setVapidDetails(
-        'mailto:lizamyshenyova@gmail.com',
-        vapid_keys.public,
-        vapid_keys.private
-    );
-
-    fs.promises.readFile(`var/subscription.json`, {encoding:"utf8"})
-        .then(function(subs) {
-          let subscription = subs.split('\n');
-          subscription.map(function(subscription) {
-            if (subscription.length > 5) {
-              subscription = JSON.parse(subscription);
-              console.log('Subscription to send to:', subscription);
-              console.log('Message to send:', new_file);
-
-                webPush.sendNotification(subscription, 'The weather report has changed')
-           .catch(function(error) {
-                console.error('sendNotification error: ', error, subscription, new_file);
-            });
-
-
+    if (new_file !== old_file) {
+      var new_data = JSON.parse(new_file)['data'];
+      var old_data = JSON.parse(old_file)['data']
+      if (new_file !== old_file){
+        for (const [state, locations] of Object.entries(new_data)) {
+          for (const [location, temp] of Object.entries(locations)) {
+            if (old_data[state][location]) {
+              var diff = temp - old_data[state][location];
+              new_data[state][location] = {"temp": temp, "diff": diff};
+            } else {
+              new_data[state][location] = {"temp": temp, "diff": "N/A"};
+            }
+          }
         }
-      });
-    })
-    .catch(function(error) {
-         console.error('Error: ', error);
-    });
-
-
-    if (new_file !== old_file)
-    {
-          console.log("Here?");
-
-      console.log(`The content of ${filename} has changed. It was a ${eventType} event.`);
-
-      var file_changes = diff.diffLines(old_file, new_file);
-      //console.log(`Here are the changes (promise!):`);
-      var new_changes = file_changes.map((change, i) => {
-        if (change.added){
-          return `<li class="ins">${change.value}</li>`;
-        }
-        if (change.removed){
-          return `<li class="del">${change.value}</li>`;
-        }
-      });
-      fileEvent.emit('changed file', new_changes.join('\n'));
+        fileEvent.emit('changed file', JSON.stringify(new_data));
+      }
     }
-    old_file = new_file
-
+    old_file = new_file;
+  })
+  .catch(function(error) {
+       console.error('Error: ', error);
   });
 });
 
-//When the `changed file` event fires, log changes locally
-fileEvent.on('changed file', function(data){
-  console.log(`The file was changed and fired an event. This are the changes:\n${data}`);
-});
-
+//a function that handles axios request for weather data
+function requestData() {
+  //requesting html from the webpage
+  axios.get('https://www.weather.gov/ilx/maxtemp')
+    .then((res) => {
+      if (res.status === 200){
+        const html = res.data;
+        //load html into cheerio
+        const $ = cheerio.load(html);
+        //loading weather infomation
+        const weatherInfo = $('pre');
+        const output = weatherInfo.html();
+        //parsing html data
+        var lines = output.split("\n").filter(line => line.length > 2);
+        var json = {};
+        var state = "";
+        json['title'] = lines.slice(0, 3).join(" ");
+        json['data'] = {};
+        lines.slice(3).forEach(line => {
+          var arr = line.split(" ").filter(word => word != '');
+          if(isNaN(arr[arr.length-1])) {
+            //state
+            state = arr.join(" ");
+            json['data'][state] = {};
+          } else {
+            //location
+            var location = arr.slice(0,arr.length-1).join(" ");
+            json['data'][state][location] = arr[arr.length-1];
+          }
+        });
+        //convert object to a json string
+        // console.log("Testing here:");
+        var weatherData = JSON.stringify(json);
+        fs.writeFile('./var/weatherData.json', weatherData, (error) => {
+            if (error) throw err;
+        });
+      }
+    })
+    .catch((error) => {
+      console.log('Fail to fetch', error);
+    })
+}
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -138,13 +150,21 @@ io.on('connection', function(socket){
   socket.on('message received', function(data){
     console.log('Client is saying:' + data)
   })
+  //when `changed file` event fires, send changes to browser
+  fileEvent.on('changed file', function(data){
+    socket.emit('diffed changes', data);
+  });
 
+  //request weather data initially
+  requestData();
+
+  //rRequest weather data every 10 minutes
+  var scheduleTask = schedule.scheduleJob('*/10 * * * *', function(){
+    requestData();
+  });
 });
 
-//when `changed file` event fires, send changes to browser
-fileEvent.on(`changed file`, function(data){
-  io.emit('diffed changes', data);
-});
+
 
 
 // catch 404 and forward to error handler
